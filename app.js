85f546f3e724443879b630bf1cf79e5383ee3b21
Bransholm <kenneth@bransholm.dk>
2023-06-01 16:02:51 +0200

    All screens with content

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 "use strict"
+
+window.addEventListener("load", start)
+
+function start() {
+    buttons();
+    console.log("Javascript is running wild")
+}
+
+function buttons() { 
+    document.querySelector("#level_complete_button").addEventListener("click", showStart);
+}
+
+function showStart() {
+    document.querySelector("#level_complete").classList.add("hidden");
+    console.log("Hidden")
+}
