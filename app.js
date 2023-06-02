23227f4b770214bb162e7c40a57dd387fc487ec4
Bransholm <kenneth@bransholm.dk>
2023-06-02 15:15:13 +0200

    animation mm.

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ let lives = 0;
 
 function start() {
     console.log("Start - Javascript is running wild");
+    showStartScreen();
     document.querySelector("#button_start_game").addEventListener("click", startGame);
     document.querySelector("#button_game_over").addEventListener("click", startGame);
     document.querySelector("#button_level_complete").addEventListener("click", showStartScreen);
+    startRocket();
+    document.querySelector("#rocket_container").addEventListener("mousedown", clickRocket);
     // showLevelComplete();
 }
 
@@ -32,4 +35,17 @@ function showLevelComplete () {
   document.querySelector("#game_over").classList.remove("hidden");
   document.querySelector("#level_complete").classList.add("hidden");
   console.log("Show game over screen");
+}
+
+function startRocket() {
+    document.querySelector("#rocket_container").classList.add("falling");
+    document.querySelector("#rocket_container").classList.add("position1");
+}
+
+function clickRocket() {
+    const rocket = this
+    // document.querySelector("#rocket_container").removeEventListener("mousedown", clickRocket);
+
+    rocket.removeEventListener("mousedown", clickRocket);
+    console.log("Rocket clicked");
 }
