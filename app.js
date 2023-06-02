192120b1942801e3f52077539779077c4bb760a8
Bransholm <kenneth@bransholm.dk>
2023-06-02 13:16:54 +0200

    buttons works

--- app.js
+++ app.js
@@ -10,10 +10,14 @@ function start() {
     document.querySelector("#button_start_game").addEventListener("click", startGame);
     document.querySelector("#button_game_over").addEventListener("click", startGame);
     document.querySelector("#button_level_complete").addEventListener("click", showStartScreen);
+    // showLevelComplete();
 }
 
 function startGame() {
-    
+  document.querySelector("#start").classList.add("hidden");
+  document.querySelector("#game_over").classList.add("hidden");
+  document.querySelector("#level_complete").classList.add("hidden");
+  console.log("Show start screen");
 }
 
 function showStartScreen () {
@@ -21,4 +25,11 @@ function showStartScreen () {
     document.querySelector("#game_over").classList.add("hidden");
     document.querySelector("#level_complete").classList.add("hidden");
     console.log("Show start screen");
+}
+
+function showLevelComplete () {
+  document.querySelector("#start").classList.add("hidden");
+  document.querySelector("#game_over").classList.remove("hidden");
+  document.querySelector("#level_complete").classList.add("hidden");
+  console.log("Show game over screen");
 }
